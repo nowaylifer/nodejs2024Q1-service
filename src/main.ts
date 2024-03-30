4d77500c9bfd08d0856584fd83ee2b02a3e37a5d
nowaylifer <nowaylifer@gmail.com>
2024-03-30 14:04:51 +0300

    feat: create logger interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  LogLevel,
+  ValidationPipe,
+} from '@nestjs/common';
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
@@ -7,14 +11,22 @@ import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 import { readFileSync } from 'node:fs';
 import { parse as parseYaml } from 'yaml';
 import { AppModule } from './app.module';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 const PORT = +process.env.PORT;
+const LOG_LEVEL = JSON.parse(process.env.LOG_LEVEL) as LogLevel[];
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: LOG_LEVEL });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector)),
+    new LoggerInterceptor(),
+  );
+
   app.useGlobalFilters(
     new PrismaClientExceptionFilter(app.get(HttpAdapterHost).httpAdapter),
   );
